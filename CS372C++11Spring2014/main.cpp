21c7e8c1028c278340352a16087a24d38bf21466
Chris Hartman <chris.m.hartman@gmail.com>
2014-01-22 09:58:13 -0900

    Initial Commit

---
+++ CS372C++11Spring2014/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CS372C++11Spring2014
+//
+//  Created by Chris Hartman on 1/22/14.
+//  Copyright (c) 2014 Chris Hartman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
