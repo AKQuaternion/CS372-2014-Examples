351a112cb0609d2939320cb33b7e36f832568c6b
Chris Hartman <chris.m.hartman@gmail.com>
2014-02-05 13:01:29 -0900

    Deleted some blank lines.

--- CS372C++11Spring2014/unique_ptr.cpp
+++ CS372C++11Spring2014/unique_ptr.cpp
@@ -18,13 +18,11 @@ using std::pair;
 #include <memory>
 using std::unique_ptr;
 
-
 template<typename T, typename... Args>
 std::unique_ptr<T> make_unique(Args&&... args)
 {
     return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
-
 
 class SpaceObject
 {
