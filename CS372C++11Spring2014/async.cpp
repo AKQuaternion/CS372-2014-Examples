9c99d5691c0d058a3cd7ee8d15e43c1fca79d2b5
Chris Hartman <chris.m.hartman@gmail.com>
2014-02-07 11:02:35 -0900

    Created async_main()

---
+++ CS372C++11Spring2014/async.cpp
@@ -0,0 +1,12 @@
+//
+//  async.cpp
+//  CS372C++11Spring2014
+//
+//  Created by Chris Hartman on 2/7/14.
+//  Copyright (c) 2014 Chris Hartman. All rights reserved.
+//
+
+void async_main()
+{
+    
+}
